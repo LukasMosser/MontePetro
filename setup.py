ff8a04ff8606d8060a2f44e02f600a143587f9fd
LukasMosser <lukas.mosser@gmail.com>
2015-01-13 17:12:42 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -73,15 +73,15 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'montepetro': [None],
-    },
+    #package_data={
+    #    'montepetro': [None],
+    #},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages.
     # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[None],
+    #data_files=[None],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
