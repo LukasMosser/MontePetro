c2ea7be1d690b581787a34ef34ccaa97efd5e953
LukasMosser <lukas.mosser@gmail.com>
2015-01-19 20:24:43 +0100

    Added functionality to models.

--- montepetro/models.py
+++ montepetro/models.py
@@ -30,5 +30,17 @@ class Model(object):
             prop.update_seed(self.seed_generator)
             self.properties[prop.name] = prop
 
+    def add_defined_properties_to_regions(self):
+        for region_name, region in self.regions.iteritems():
+            for property_name, property in self.properties.iteritems():
+                region.add_property(property)
+
+    def run(self, config):
+        for region_name, region in self.regions.iteritems():
+            region_config = config[region_name]
+            for property_name, property in region.properties.iteritems():
+                regional_property_config = region_config[property_name]
+                property.generate_values(**regional_property_config)
+
     def dump(self):
         pass
