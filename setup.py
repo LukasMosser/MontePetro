e09d450849371215cd87af7fc70dfb776271bfcc
LukasMosser <lukas.mosser@gmail.com>
2015-01-22 18:22:57 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -14,40 +14,20 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2',
+    version='0.3',
 
     description='Probabilistic Reserve Estimates in Python',
     long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/LukasMosser/MontePetro',
-    download_url = 'https://github.com/LukasMosser/MontePetro/tarball/0.2',
+    download_url = 'https://github.com/LukasMosser/MontePetro/tarball/0.3',
     # Author details
     author='Lukas Mosser',
     author_email='lukas.mosser@gmail.com',
 
     # Choose your license
     license='GPL',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GPL License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7'
-    ],
 
     # What does your project relate to?
     keywords=['montecarlo', 'probabilistic', 'oil', 'gas'],
