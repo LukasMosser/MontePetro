cfab6de2335bcea23fdea25360deee2f5a2cd17f
LukasMosser <lukas.mosser@gmail.com>
2015-01-22 17:57:59 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.1',
 
     description='Probabilistic Reserve Estimates in Python',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/lukasmosser/montepetro',
-
+    url='https://github.com/LukasMosser/MontePetro',
+    download_url = 'https://github.com/LukasMosser/MontePetro/tarball/0.1',
     # Author details
     author='Lukas Mosser',
     author_email='lukas.mosser@gmail.com',
@@ -50,7 +50,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='montecarlo probabilistic oilandgas',
+    keywords=['montecarlo', 'probabilistic', 'oil', 'gas'],
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -68,27 +68,5 @@ setup(
     extras_require = {
         'dev': ['check-manifest'],
         'test': ['coverage', 'nose'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={
-    #    'montepetro': [None],
-    #},
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[None],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'montepetro=montepetro:main',
-        ],
-    },
+                     }
 )
